6532d6febeab189b0c8092f4e6ba3534e316ba14
KeerhemFR <keerhem.guillaume@hotmail.fr>
2022-01-20 11:27:56 +0100

    Imported LocateUser component in ServiceMap

--- src/ServiceMap/index.jsx
+++ src/ServiceMap/index.jsx
@@ -1,17 +1,23 @@
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { MapContainer, TileLayer } from 'react-leaflet';
+import LocateUser from '../LocateUser';
 import SServiceMap from './style';
 
 export default function ServiceMap() {
+  // function handleLocate() {
+  //   return setFindUser(!findUser);
+  // }
+
   return (
     <SServiceMap>
+      {/* <button type="button" onClick={handleLocate}>
+        Se localiser
+      </button> */}
       <MapContainer center={[50.629, 2.987]} zoom={15} scrollWheelZoom>
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <Marker position={[50.629, 2.987]}>
-          <Popup>Services</Popup>
-        </Marker>
+        <LocateUser />
       </MapContainer>
     </SServiceMap>
   );
